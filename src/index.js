4ac9bdd88314784ca4d2a4fe83745a2c76dc9cd2
Yaroslav Turkov <turkov988@gmail.com>
2023-10-28 22:53:30 +0300

    docs: add a variables in func v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+  const small = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const big = [];
+  const bigest = [];
+  const numberToString = [];
+  const bigNumber1 = number % 10;
+  const bigNumber2 = number % 100;
+
+
+
 }
