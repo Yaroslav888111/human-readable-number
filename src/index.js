5f4e9077844a1d6d8a8eafe21731f59831147de1
Yaroslav Turkov <turkov988@gmail.com>
2023-10-28 23:02:20 +0300

    docs: add 1st if

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = function toReadable (number) {
   const small = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const big = [];
-  const bigest = [];
-  const numberToString = [];
+  const big = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const bigest = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const numberToString = number.toString();
   const bigNumber1 = number % 10;
   const bigNumber2 = number % 100;
 
+  if (number < 10) {
+    write = small[number];
+  } else if (number >= 20 && number < 100) {
+    if (small ===0) {
+        write =
+    }
+  }
 
 
 }
