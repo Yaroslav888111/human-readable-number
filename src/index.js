49173436970ce4612a86b0839425875f649078c4
Yaroslav Turkov <turkov988@gmail.com>
2023-10-29 21:03:09 +0300

    docs: add if case for number_1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 module.exports = function toReadable (number) {
-  const small = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const big = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  const bigest = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const num0_9 = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const num11_19 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const num10_90 = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   const numberToString = number.toString();
-  const bigNumber1 = number % 10;
-  const bigNumber2 = number % 100;
+  const num_10 = number % 10;
+  const num_100 = number % 100;
 
   if (number < 10) {
-    write = small[number];
-  } else if (number >= 20 && number < 100) {
-    if (small ===0) {
-        write =
-    }
+    return num0_9[number];
   }
-
-
+  if (number > 10 && number < 20) {
+    return num11_19[num_10 - 1];
+  }
+  if (num_10 === 0 && number < 100) {
+    return num10_90[numberToString[0] - 1];
+  }
+  if (num_10 !== 0 && number < 100) {
+    return `${num10_90[numberToString[0] - 1]} ${num0_9[num_10]}`;
+  }
+  if (number >= 100) {
+    let str = `${num0_9[numberToString[0]]} num_100`;
+  }
+  if (num_100 === 0) return str;
+  if (num_100 !== 0) {
+    if (num_10 === 0) return `${str} ${num10_90[numberToString[1] - 1]}`;
+  }
 }
